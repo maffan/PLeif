fe33eb6520833fdaee47b305a6bd96f9243180ab
maffan <marcus.flyckt@gmail.com>
2015-02-04 08:26:37 +0100

    added monster class

---
+++ src/Models/Monster.java
@@ -0,0 +1,25 @@
+package Models;
+
+/**
+ * Created by Marcus on 2/4/2015.
+ */
+public class Monster extends Entity {
+    private Stats stats;
+
+    public Monster(){
+
+    }
+
+    public Monster(Stats stats){
+        this.stats = stats;
+    }
+    @Override
+    public void doTurn() {
+
+    }
+
+    @Override
+    public String getType() {
+        return "Monster";
+    }
+}
