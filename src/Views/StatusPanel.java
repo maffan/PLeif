89c308ba260652fdcbd05e8b5702e7a74c474f05
maffan <marcus.flyckt@gmail.com>
2015-02-04 10:33:40 +0100

    updated uml

--- src/Views/StatusPanel.java
+++ src/Views/StatusPanel.java
@@ -7,9 +7,13 @@ import java.awt.*;
  * Created by Marcus on 2/4/2015.
  */
 public class StatusPanel extends JPanel{
+    private InputPanel inputPanel;
+    private OutputPanel outputPanel;
     public StatusPanel(){
         add(new JLabel("Status"));
         setPreferredSize(new Dimension(500,100));
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setLayout(new BorderLayout());
+
     }
 }
