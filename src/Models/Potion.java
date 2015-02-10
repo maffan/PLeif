2475bb1c3824da66cf018a77862ff5be475486b1
maffan <marcus.flyckt@gmail.com>
2015-02-10 13:28:21 +0100

    added constructor for Potion

--- src/Models/Potion.java
+++ src/Models/Potion.java
@@ -4,4 +4,8 @@ package Models;
  * Created by Marcus on 2/3/2015.
  */
 public abstract class Potion implements Usable {
+    private Stats stats;
+    public Potion(Stats stats) {
+        this.stats = stats;
+    }
 }
