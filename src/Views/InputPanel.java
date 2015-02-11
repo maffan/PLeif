16f217494918fa20c3a259fc39fa7e588e847599
maffan <marcus.flyckt@gmail.com>
2015-02-11 19:05:47 +0100

    added console in inputpanel

--- src/Views/InputPanel.java
+++ src/Views/InputPanel.java
@@ -7,8 +7,24 @@ import java.awt.*;
  * Created by Marcus on 2/4/2015.
  */
 public class InputPanel extends JPanel {
+    private JTextField inputField;
+    private JButton submitButton;
     public InputPanel(){
-        add(new JLabel("Input"));
+        inputField = new JTextField(30);
+        submitButton = new JButton("Submit");
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(inputField,constraints);
+        
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        add(submitButton,constraints);
     }
 }
