8480763a83dc451b6ac6393fb5263595733383c0
johnss0n <johnsson3.14@gmail.com>
2015-02-26 17:44:23 +0100

    Ficed bug in Entity-class
    * Fixed constructor in Entity, BoardPanel draws items correct

--- src/Models/Item.java
+++ src/Models/Item.java
@@ -15,7 +15,7 @@ public class Item extends Entity {
     }
 
     public Item(int x, int y, String name, Stats stats, SpriteID id){
-        super(x, y, name, SpriteID.None);
+        super(x, y, name, id);
         this.stats = stats;
     }
 
