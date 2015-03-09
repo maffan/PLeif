f6733dd71068ee61486a47b63009718b3d198b9d
laepen <tobnie@student.chalmers.se>
2015-03-09 15:34:30 +0100

    JavaDoc
    JavaDoc för models item

--- src/Models/Item.java
+++ src/Models/Item.java
@@ -2,19 +2,31 @@ package Models;
 
 /**
  * Created by Marcus on 2/3/2015.
+ *
  */
+
 public class Item extends Entity {
     private Stats stats;
 
     public Item(){
 
     }
-
+    
+    /**
+     * @param x value
+     * @param y value
+     * @param name unique name for item
+     * @param stats 
+     * @param id SpriteID of item
+     */
     public Item(int x, int y, String name, Stats stats, SpriteID id){
         super(x, y, name, id);
         this.stats = stats;
     }
 
+    /**
+     * @return stats for the item.
+     */
     public Stats getStats(){
         return stats;
     }
